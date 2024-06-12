630c16e8eb0aae5b4f3a4d559b559ee1e28ce4d4
Tyler J Cvetan <tyler.cvetan@gmail.com>
2024-06-11 20:44:32 -0500

    feat: all app-core to pull from sdk

--- packages/app-core/src/App.tsx
+++ packages/app-core/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Counter } from '@somethingsexy/sdk'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -25,6 +26,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <Counter />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
---
+++ packages/sdk/src/component/counter/Counter.tsx
@@ -0,0 +1,4 @@
+
+export const Counter = () => {
+  return <div>Foo</div>
+}
